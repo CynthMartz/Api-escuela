b8ee42a3a2321c98eaa1e8eaae659c7030974628
CynthMartz <carol.martz21@gmail.com>
2023-12-14 21:58:28 -0300

    Cambiando puerto

--- server.js
+++ server.js
@@ -3,25 +3,24 @@ const express = require('express')
 const cors = require("cors")
 const mongoose = require('mongoose')
 const dbConnect = require ('./config/mongo')
-
-
-
 const app = express()
+
 app.use(cors())
 app.use(express.json())
 
 
-dbConnect()
+
 
 const port = process.env.PORT || 3000
 
 
 /*API rest*/ 
-app.use("/api", require("./routes"));
+app.use("/api", require("./routes"))
 
 app.listen(port, () => {
     console.log(`http://localhost:${port}`)
 });
+dbConnect()
 
 //renombrar alumno
 const fs = require ('fs');
