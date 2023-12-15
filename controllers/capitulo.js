940afb5f6acce274e03dec30325b49b350f3ebe2
CynthMartz <carol.martz21@gmail.com>
2023-12-14 21:57:52 -0300

    Creando controllers

---
+++ controllers/capitulo.js
@@ -0,0 +1,49 @@
+const {capituloModel} = require ('../models')
+
+
+/** obtener lista de la base de datos
+@param{*} req
+@param{*} res
+*/
+
+const getItems = async (req, res)=> {
+    const data = await capituloModel.find ({});
+    res.send ({data})
+};
+
+/** obtener un detalle
+@param{*} req
+@param{*} res
+*/
+const getItem = (req, res)=> {};
+
+
+
+/** Insertar un registro
+@param{*} req
+@param{*} res
+*/
+const createItem = async (req, res) => {
+    const { body } = req
+    console.log(body)
+    const data = await capituloModel.create(body)
+    res.send({data})
+};
+
+
+/** Actualizar un registro
+@param{*} req
+@param{*} res
+*/
+const updateItem = (req, res)=> {};
+
+
+
+
+/** Eliminar un registro
+@param{*} req
+@param{*} res
+*/
+const deleteItem = (req, res)=> {};
+
+module.exports = { getItems, getItem, createItem, updateItem, deleteItem }
